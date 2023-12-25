54d2ddbcda1246e881082678c2bab76357948f88
Iulia2191 <ibocaniciu@gmail.com>
2023-12-25 15:49:23 +0100

    update script

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ const jokes = [
   ["Why did the scarecrow win an award?", "Because he was outstanding in his field!"],
   ["How does a penguin build its house?", "Igloos it together!"],
   ["What did one wall say to the other wall?", "I'll meet you at the corner!"],
-  ["Why don't scientists trust atoms?", "Because they make up everything!"]
+  ["Why don't scientists trust atoms?", "Because they make up everything!"],
+  ["What do you call a fake noodle?", "An impasta."],
+  ["Why did the bicycle fall over?", "Because it was two-tired."],
+  ["How does a penguin drink its cola?", "On the rocks."],
 ];
 
 const responses = [
   "Sorry, I'm out of jokes for now!",
   "I need some time to think of more jokes.",
-  "Hmm, I'm feeling a bit shy today. No more jokes!"
- 
+  "Hmm, I'm feeling a bit shy today. No more jokes!",
+  "Let's switch topics. Any preferences?"
 ];
 
 const chatContent = document.querySelector(".chat-content");
-const jokeButton = document.getElementById("joke-button"); // 
+const jokeButton = document.getElementById("joke-button");
 
 let jokeIndex = 0;
+let responseIndex = 0;
 
 function appendBotMessage(messageText) {
   const messageDiv = document.createElement("div");
@@ -53,10 +57,9 @@ function requestJoke() {
   setTimeout(function () {
     if (jokeIndex < jokes.length) {
       appendBotMessage(jokes[jokeIndex][0]);
-      jokeIndex++;
     } else {
-      appendBotMessage(getRandomResponse());
-      jokeButton.style.display = "none"; // Hide the button
+      appendBotMessage(getNextResponse());
+      jokeButton.style.display = "none";
       return;
     }
   }, 1500);
@@ -65,20 +68,20 @@ function requestJoke() {
       appendBotMessage(jokes[jokeIndex][1]);
       jokeIndex++;
     } else {
-      appendBotMessage(getRandomResponse());
-      jokeButton.style.display = "none"; // Hide the button
+      appendBotMessage(getNextResponse());
+      jokeButton.style.display = "none";
     }
   }, 2000);
 }
 
-function getRandomResponse() {
-  const index = Math.floor(Math.random() * responses.length);
-  return responses[index];
+function getNextResponse() {
+  const response = responses[responseIndex];
+  responseIndex = (responseIndex + 1) % responses.length;
+  return response;
 }
 
 function scrollToBottom() {
   chatContent.scrollTop = chatContent.scrollHeight;
 }
 
-// Trigger the requestJoke function
 requestJoke();
